573758b944f4d94d711e50853ef4620d6d01da37
Hendri Arifin <hendri.cacem@gmail.com>
2024-06-10 04:29:16 +0000

    remove rerun

--- main_app.py
+++ main_app.py
@@ -132,4 +132,4 @@ with right_column:
         captcha_text, captcha_image = generate_captcha()
         st.session_state['captcha_text'] = captcha_text
         st.session_state['captcha_image'] = captcha_image
-        st._rerun()
+        #st._rerun()
